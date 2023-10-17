2bfb72f2e7d71e60a31376bdc0d1cce42881be47
AlbaSly <arias.brando.raxel@gmail.com>
2023-10-17 15:35:08 -0600

    Create SASS styles and some data

---
+++ app/_data/index.ts
@@ -0,0 +1,4 @@
+export * from './routes';
+export * from './services';
+export * from './technologies';
+export * from './contact-links';
---
+++ app/_data/services.ts
@@ -0,0 +1,23 @@
+export const SERVICES: Array<IRaxService> = [
+  {
+    label: "Web Applications Development",
+    src: "",
+  },
+  {
+    label: "Information Systems Development",
+    src: "",
+  },
+  {
+    label: "Database Design",
+    src: "",
+  },
+  {
+    label: "User Interfaces Design",
+    src: "",
+  },
+];
+
+interface IRaxService {
+  label: string;
+  src: string;
+}
